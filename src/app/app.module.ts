0f5e77a3f8423c83fb261a9dc50f57a903b0bd8d
Aled Wassell <aled@phntms.com>
2023-01-18 22:02:37 +0000

    Animate the list ites so when hovered they expand.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import {AppComponent} from './app.component';
-import {NavigationComponent} from './components/navigation/navigation.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {MainComponent} from './components/main/main.component';
-import {AppRoutingModule} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { AppRoutingModule } from './app-routing.module';
 // import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { AngularFireModule } from '@angular/fire/compat';
 import { ResumeComponent } from './components/resume/resume.component';
 import { SafePipe } from './pipes/safe.pipe';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { EntityDataModule } from '@ngrx/data';
 import { entityConfig } from './entity-metadata';
 import { EffectsModule } from '@ngrx/effects';
@@ -21,29 +22,29 @@ import { StoreModule } from '@ngrx/store';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavigationComponent,
-        FooterComponent,
-        MainComponent,
-        ResumeComponent,
-        SafePipe,
-    ],
-    imports: [
-        BrowserModule,
-        CommonModule,
-        AppRoutingModule,
-        HttpClientModule,
-        AngularFireModule.initializeApp(environment.firebase),
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideFirestore(() => getFirestore()),
-        StoreModule.forRoot({}),
-        EffectsModule.forRoot([]),
-        EntityDataModule.forRoot(entityConfig),
-        PdfViewerModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    MainComponent,
+    ResumeComponent,
+    SafePipe,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    EntityDataModule.forRoot(entityConfig),
+    PdfViewerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
