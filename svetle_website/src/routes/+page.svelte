a8e89dc1052086913ad5b637294116c037a84c9f
aledwassell <aledwassell@googlemail.com>
2025-10-07 07:01:44 +0100

    set up dark theme and do page layout

--- svetle_website/src/routes/+page.svelte
+++ svetle_website/src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+    import { browser } from '$app/environment';
+
     let links = [
         {
             href: "/resume",
@@ -9,18 +12,73 @@
             text: "projects"
         }
     ];
+
+    let isDark = false;
+
+    onMount(() => {
+        if(!browser) return;
+
+        const storedTheme = localStorage.getItem('theme');
+        switch (storedTheme) {
+            case 'dark':
+                isDark = true;
+                document.documentElement.classList.add('dark');
+                break;
+            case 'light':
+                isDark = false;
+                document.documentElement.classList.remove('dark');
+                break;
+            default:
+                isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                document.documentElement.classList.toggle('dark', isDark);
+        }
+    });
+
+    function toggleDarkMode() {
+        isDark = !isDark;
+        
+        if (browser) {
+            document.documentElement.classList.toggle('dark', isDark);
+            const theme = isDark ? 'dark' : 'light';
+            localStorage.setItem('theme', theme);
+        }
+    }
 </script>
 
-<div class="flex dark:bg-gray-800 bg-white text-gray-800 dark:text-white flex-col items-center justify-center h-screen">
-    <div class="flex flex-col items-center justify-center">
-        <p class="text-2xl">i'm aled, a software developer from warwick, uk</p>
-        {#each links as link}
-            <a class="text-2xl p-5 dark:border-white border-gray-800 border-2" href={link.href}>{link.text}</a>
-        {/each}
-        <section class="flex flex-row items-center justify-center">
+<div class="flex dark:bg-gray-800 bg-white text-gray-800 dark:text-white flex-col items-center justify-center transition-colors duration-400 h-screen">
+
+
+    <div class="flex flex-col lg:w-3xl w-full space-y-5">
+        <h1 class="text-2xl">i'm aled wassell<span class="text-pink-600 dark:text-pink-400">/</span></h1>
+        <ul class="w-full">
+            {#each links as link, last}
+                <li class="text-2xl dark:border-white border-gray-800 w-full flex justify-end {last ? 'border-b-0' : 'border-b-2'}">
+                    <a href={link.href} target="_blank" class="hover:text-emerald-600 dark:hover:text-emerald-600 hover:bg-gray-50 dark:hover:bg-gray-700 py-5 w-full transition-colors duration-400">{link.text}</a>
+                </li>
+            {/each}
+        </ul>
+
+        <section class="flex items-center justify-between">
+            <button
+                onclick={toggleDarkMode}
+                class="cursor-pointer"
+                aria-label="Toggle dark mode"
+            >
+                {#if isDark}
+                    <span class="solar--sun-bold-duotone"></span>
+                {:else}
+                    <span class="solar--moon-stars-bold"></span>
+                {/if}
+            </button>
             
-            <a href="https://www.linkedin.com/in/aledwassell">linkedin</a>
-            <a href="https://github.com/aledwassell">github</a>
+            <ul class="flex flex-row">
+                <li>
+                    <a href="https://www.linkedin.com/in/aledwassell" class="hover:bg-blue-500 hover:text-white transition-colors duration-400" target="_blank">linkedin/</a>
+                </li>
+                <li>
+                    <a href="https://github.com/aledwassell" class="hover:bg-green-700 hover:text-white transition-colors duration-400" target="_blank">github/</a>
+                </li>
+            </ul>
         </section>
     </div>
 </div>
