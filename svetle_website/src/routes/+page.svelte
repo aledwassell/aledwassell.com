0d1dac65253439e128a87ed993ee196dc414ca13
aledwassell <aledwassell@googlemail.com>
2025-10-07 05:45:53 +0100

    Add the svetle website directory

---
+++ svetle_website/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let links = [
+        {
+            href: "/resume",
+            text: "resume"
+        },
+        {
+            href: "/projects",
+            text: "projects"
+        }
+    ];
+</script>
+
+<div class="flex dark:bg-gray-800 bg-white text-gray-800 dark:text-white flex-col items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center">
+        <p class="text-2xl">i'm aled, a software developer from warwick, uk</p>
+        {#each links as link}
+            <a class="text-2xl p-5 dark:border-white border-gray-800 border-2" href={link.href}>{link.text}</a>
+        {/each}
+        <section class="flex flex-row items-center justify-center">
+            
+            <a href="https://www.linkedin.com/in/aledwassell">linkedin</a>
+            <a href="https://github.com/aledwassell">github</a>
+        </section>
+    </div>
+</div>
