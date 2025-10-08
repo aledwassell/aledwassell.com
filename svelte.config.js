e5fb4c85ade5f554e2e139edbda098e6cb48160c
aledwassell <aledwassell@googlemail.com>
2025-10-08 20:50:41 +0100

    Set up for deploy GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/aledwassell.com' : ''
+			base: process.env.GITHUB_ACTIONS ? '/aledwassell.com' : ''
 		}
 	}
 };
